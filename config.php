66f9a57e1b827f0871d7c2e9911c5fc1da0d204b
Abdul Fakhri <abf.ca@hotmail.com>
2022-01-23 21:04:33 -0800

    update

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'root');
-define('DB_NAME', 'crm_steve');
+define('DB_USER','u559678163_lckhubu');
+define('DB_PASS' ,'!@#123qweasdZXC');
+define('DB_NAME', 'u559678163_lckhub');
 $conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
